3a21a89c643663f04a1a2ef75e58252d9cc3ef5f
Ray Chung <ray20020809@gmail.com>
2021-07-12 14:57:15 +0800

    not verify domain yet (add middleware)

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/ray1422/YAM-api/room"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8080", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	apiRouter := router.Group("/api")
 	room.RegisterRouter(apiRouter.Group("/room"))
 	router.Run()
